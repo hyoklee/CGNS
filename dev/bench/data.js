779e7ccf52a394e45ca8b65fb988597bb967f7ee
github-action-benchmark <github@users.noreply.github.com>
2025-09-18 22:25:49 +0000

    add CGNS HDF5 Performance (customSmallerIsBetter) benchmark result for 405a39e81759453a66cbabebeac36faf755d8c5c

---
+++ dev/bench/data.js
@@ -0,0 +1,402 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1758234349836,
+  "repoUrl": "https://github.com/hyoklee/CGNS",
+  "entries": {
+    "CGNS HDF5 Performance": [
+      {
+        "commit": {
+          "author": {
+            "email": "hyoklee@hdfgroup.org",
+            "name": "H. Joe Lee",
+            "username": "hyoklee"
+          },
+          "committer": {
+            "email": "hyoklee@hdfgroup.org",
+            "name": "H. Joe Lee",
+            "username": "hyoklee"
+          },
+          "distinct": true,
+          "id": "405a39e81759453a66cbabebeac36faf755d8c5c",
+          "message": "chore: delete json",
+          "timestamp": "2025-09-18T17:20:32-05:00",
+          "tree_id": "b07895fbd48794099e7cfc096b7d47990f49ab12",
+          "url": "https://github.com/hyoklee/CGNS/commit/405a39e81759453a66cbabebeac36faf755d8c5c"
+        },
+        "date": 1758234348887,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Test 1: Small Mesh Test (32K elements, serial) - Elements Processed",
+            "value": 32000,
+            "unit": "elements"
+          },
+          {
+            "name": "Test 2: Medium Mesh Test (128K elements, serial) - Elements Processed",
+            "value": 128000,
+            "unit": "elements"
+          },
+          {
+            "name": "Test 3: Large Mesh Test (512K elements, serial) - Elements Processed",
+            "value": 512000,
+            "unit": "elements"
+          },
+          {
+            "name": "Test 4: Very Large Mesh Test (1M elements, serial) - Elements Processed",
+            "value": 1000000,
+            "unit": "elements"
+          },
+          {
+            "name": "Serial HDF5 - Total Runtime",
+            "value": 0.025,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Total Write Time",
+            "value": 0.007,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Total Read Time",
+            "value": 0.002,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Write Coordinates",
+            "value": 0.002,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Write Elements",
+            "value": 0.002,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Write Fields",
+            "value": 0.002,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Write Arrays",
+            "value": 0.001,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Read Coordinates",
+            "value": 0.001,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Read Elements",
+            "value": 0.001,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Data Size Fields",
+            "value": 3,
+            "unit": "MB"
+          },
+          {
+            "name": "Serial HDF5 - Data Size Arrays",
+            "value": 2,
+            "unit": "MB"
+          },
+          {
+            "name": "Serial HDF5 - Total Data Size",
+            "value": 5,
+            "unit": "MB"
+          },
+          {
+            "name": "Serial HDF5 - Write Throughput",
+            "value": 714.2857142857142,
+            "unit": "MB/s"
+          },
+          {
+            "name": "Serial HDF5 - Read Throughput",
+            "value": 2500,
+            "unit": "MB/s"
+          },
+          {
+            "name": "Serial HDF5 - Total Runtime",
+            "value": 0.027,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Total Write Time",
+            "value": 0.007,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Total Read Time",
+            "value": 0.002,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Write Coordinates",
+            "value": 0.002,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Write Elements",
+            "value": 0.002,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Write Fields",
+            "value": 0.002,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Write Arrays",
+            "value": 0.001,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Read Coordinates",
+            "value": 0.001,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Read Elements",
+            "value": 0.001,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Data Size Fields",
+            "value": 3,
+            "unit": "MB"
+          },
+          {
+            "name": "Serial HDF5 - Data Size Arrays",
+            "value": 2,
+            "unit": "MB"
+          },
+          {
+            "name": "Serial HDF5 - Total Data Size",
+            "value": 5,
+            "unit": "MB"
+          },
+          {
+            "name": "Serial HDF5 - Write Throughput",
+            "value": 714.2857142857142,
+            "unit": "MB/s"
+          },
+          {
+            "name": "Serial HDF5 - Read Throughput",
+            "value": 2500,
+            "unit": "MB/s"
+          },
+          {
+            "name": "Serial HDF5 - Total Runtime",
+            "value": 0.027,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Total Write Time",
+            "value": 0.007,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Total Read Time",
+            "value": 0.002,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Write Coordinates",
+            "value": 0.002,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Write Elements",
+            "value": 0.002,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Write Fields",
+            "value": 0.002,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Write Arrays",
+            "value": 0.001,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Read Coordinates",
+            "value": 0.001,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Read Elements",
+            "value": 0.001,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Data Size Fields",
+            "value": 3,
+            "unit": "MB"
+          },
+          {
+            "name": "Serial HDF5 - Data Size Arrays",
+            "value": 2,
+            "unit": "MB"
+          },
+          {
+            "name": "Serial HDF5 - Total Data Size",
+            "value": 5,
+            "unit": "MB"
+          },
+          {
+            "name": "Serial HDF5 - Write Throughput",
+            "value": 714.2857142857142,
+            "unit": "MB/s"
+          },
+          {
+            "name": "Serial HDF5 - Read Throughput",
+            "value": 2500,
+            "unit": "MB/s"
+          },
+          {
+            "name": "Serial HDF5 - Total Runtime",
+            "value": 0.027,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Total Write Time",
+            "value": 0.007,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Total Read Time",
+            "value": 0.002,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Write Coordinates",
+            "value": 0.002,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Write Elements",
+            "value": 0.002,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Write Fields",
+            "value": 0.002,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Write Arrays",
+            "value": 0.001,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Read Coordinates",
+            "value": 0.001,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Read Elements",
+            "value": 0.001,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Data Size Fields",
+            "value": 3,
+            "unit": "MB"
+          },
+          {
+            "name": "Serial HDF5 - Data Size Arrays",
+            "value": 2,
+            "unit": "MB"
+          },
+          {
+            "name": "Serial HDF5 - Total Data Size",
+            "value": 5,
+            "unit": "MB"
+          },
+          {
+            "name": "Serial HDF5 - Write Throughput",
+            "value": 714.2857142857142,
+            "unit": "MB/s"
+          },
+          {
+            "name": "Serial HDF5 - Read Throughput",
+            "value": 2500,
+            "unit": "MB/s"
+          },
+          {
+            "name": "Serial HDF5 - Total Runtime",
+            "value": 0.027,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Total Write Time",
+            "value": 0.007,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Total Read Time",
+            "value": 0.002,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Write Coordinates",
+            "value": 0.002,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Write Elements",
+            "value": 0.002,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Write Fields",
+            "value": 0.002,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Write Arrays",
+            "value": 0.001,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Read Coordinates",
+            "value": 0.001,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Read Elements",
+            "value": 0.001,
+            "unit": "seconds"
+          },
+          {
+            "name": "Serial HDF5 - Data Size Fields",
+            "value": 3,
+            "unit": "MB"
+          },
+          {
+            "name": "Serial HDF5 - Data Size Arrays",
+            "value": 2,
+            "unit": "MB"
+          },
+          {
+            "name": "Serial HDF5 - Total Data Size",
+            "value": 5,
+            "unit": "MB"
+          },
+          {
+            "name": "Serial HDF5 - Write Throughput",
+            "value": 714.2857142857142,
+            "unit": "MB/s"
+          },
+          {
+            "name": "Serial HDF5 - Read Throughput",
+            "value": 2500,
+            "unit": "MB/s"
+          }
+        ]
+      }
+    ]
+  }
+}
